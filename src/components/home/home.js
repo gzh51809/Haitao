8ac925c4ab02853b5a2624eb5893db3393c16b03
xincheng <2531529901@qq.com>
2019-02-05 23:02:34 +0800

    home...

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,8 +5,10 @@ import { Carousel } from 'antd';
 import 'antd/dist/antd.css';
 import axios from 'axios';
 // import fetch from 'fetch';
+import Arrival from './arrival';
+import Mother from './mother';
 
-import '../style/home.css';
+import '../style/home/home.css';
 
 class Home extends React.Component {
     constructor() {
@@ -89,7 +91,79 @@ class Home extends React.Component {
                         <div>英国好评明星产品</div>
                     </div>
                     <img src="https://resource.51taouk.com/AdminImages/Content1/1/5b00646c-d4ef-446b-8f19-94c4aaaaf144.png?imageMogr2/format/webp/thumbnail/x536" />
-                    <div className="products"></div>
+                    <div className="products">
+                        <div className="item">
+                            <div className="productImg">
+                                <img src="https://resource.51taouk.com/AdminImages/Product/6/979a96f9-caca-4034-b55a-b25669b54f26.jpg" />
+                            </div>
+                            <p className="text">【包邮包税】MARVEL成人低脂奶粉340gX4</p>
+                            <p className="price">
+                                <span className="active">￥</span><span className="active">20.41</span>
+                                <span className="del"><del>￥30.00</del></span>
+                            </p>
+                        </div>
+                        <div className="item">
+                            <div className="productImg">
+                                <img src="https://resource.51taouk.com/AdminImages/Product/6/2ae08a10-5420-438c-9d85-2440213e8751.jpg" />
+                            </div>
+                            <p className="text">【包邮包税】4罐 英国原装爱他美 白金版 2段 6-12个月 奶粉800G</p>
+                            <p className="price">
+                                <span className="active">￥</span><span className="active">96.00</span>
+                                <span className="del"><del>￥101.00</del></span>
+                            </p>
+                        </div>
+                        <div className="item">
+                            <div className="productImg">
+                                <img src="https://resource.51taouk.com/AdminImages/Product/6/f659c903-b05a-4bec-8a4b-49f88940a690.jpg" />
+                            </div>
+                            <p className="text">【包邮包税】6罐 英国原装爱他美 白金版 2段 6-12个月 奶粉800G</p>
+                            <p className="price">
+                                <span className="active">￥</span><span className="active">144.00</span>
+                                <span className="del"><del>￥149.00</del></span>
+                            </p>
+                        </div>
+                        <div className="item">
+                            <div className="productImg">
+                                <img src="https://resource.51taouk.com/AdminImages/Product/6/09cba6ac-1519-4597-b365-0d2c3b32d6fc.jpg" />
+                            </div>
+                            <p className="text">【包邮包税】6罐 英国原装爱他美 白金版 3段 1-2岁 奶粉800G</p>
+                            <p className="price">
+                                <span className="active">￥</span><span className="active">144.00</span>
+                                <span className="del"><del>￥160.00</del></span>
+                            </p>
+                        </div>
+                        <div className="item">
+                            <div className="productImg">
+                                <img src="https://resource.51taouk.com/AdminImages/Product/6/d9c7dea8-3ffb-4e8e-9685-405932760a92.jpg" />
+                            </div>
+                            <p className="text">【包邮包税】4罐 英国原装爱他美 白金版 3段 1-2岁 奶粉800G</p>
+                            <p className="price">
+                                <span className="active">￥</span><span className="active">96.00</span>
+                                <span className="del"><del>￥110.00</del></span>
+                            </p>
+                        </div>
+                        <div className="item">
+                            <div className="productImg">
+                                <img src="https://resource.51taouk.com/AdminImages/Product/6/fc6ca253-9df7-4d40-80a1-9a30c91e809e.jpg" />
+                            </div>
+                            <p className="text">【包邮包税】ManukaPharm 70抗菌值麦卢卡蜂蜜250g</p>
+                            <p className="price">
+                                <span className="active">￥</span><span className="active">24.43</span>
+                                <span className="del"><del>￥33.99</del></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <Arrival></Arrival>
+                <Mother></Mother>
+                <Mother></Mother>
+                <Mother></Mother>
+                <Mother></Mother>
+                <Mother></Mother>
+                <div className="end">
+                    <div className="one"></div>
+                    <span>没有更多了</span>
+                    <div className="two"></div>
                 </div>
             </div>
         )
---
+++ src/components/home/mother.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { Icon } from 'antd';
+
+import '../style/home/mother.css';
+
+class Mother extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div className="mother">
+                <div className="title">
+                    <Icon type="trophy" theme="filled" style={{ color: '#ff7c6b' }} />
+                    <span>母婴用品</span>
+                </div>
+                <img src="https://resource.51taouk.com/AdminImages/Content1/1/62d9a5a4-2dd5-48d8-b20e-248535f26cf5.png?imageMogr2/format/webp/thumbnail/x536" />
+                <div className="products">
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/2886cba4-b9ff-4659-87a0-9abd99f4048b.jpg" />
+                        </div>
+                        <p className="text">【包邮包税】thisworks助眠旅行套装50ml</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">57.59</span>
+                            <span className="del"><del>￥69.16</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/df270ab6-aa08-41f0-9e47-5ba145836c8b.jpg" />
+                        </div>
+                        <p className="text">【包邮包税】安娜贝尔卡摩尔有机香蕉蓝莓椰奶果泥100g+香蕉香梨桃子果泥100g+西梅苹果香梨果泥100g+草莓香蕉椰奶果泥100g+婴儿意面250g</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">14.01</span>
+                            <span className="del"><del>￥15.85</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/aba90855-247a-4182-8c45-ad8af76b39a5.jpg" />
+                        </div>
+                        <p className="text">HAWKINS&BRIMBLE控油保湿霜100ml</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">10.99</span>
+                            <span className="del"><del>￥14.95</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/39661c59-e806-470a-980b-ec0ce611695f.jpg" />
+                        </div>
+                        <p className="text">【包邮包税】thisworks抗皱极润保湿霜48ml</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">54.18</span>
+                            <span className="del"><del>￥46.00</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/a708e4a2-befc-4c71-b9dc-f53134730e04.jpg" />
+                        </div>
+                        <p className="text">NATURES BEST养颜片60片</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">14.58</span>
+                            <span className="del"><del>￥18.95</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/76059428-ed5e-41c3-8e37-cd230ec0799d.jpg" />
+                        </div>
+                        <p className="text">【包邮包税】waitrose2019新年大礼包3号</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">20.07</span>
+                            <span className="del"><del>￥24.08</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/76059428-ed5e-41c3-8e37-cd230ec0799d.jpg" />
+                        </div>
+                        <p className="text">【包邮包税】waitrose2019新年大礼包3号</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">20.07</span>
+                            <span className="del"><del>￥24.08</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/76059428-ed5e-41c3-8e37-cd230ec0799d.jpg" />
+                        </div>
+                        <p className="text">【包邮包税】waitrose2019新年大礼包3号</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">20.07</span>
+                            <span className="del"><del>￥24.08</del></span>
+                        </p>
+                    </div>
+                    <div className="item">
+                        <div className="productImg">
+                            <img src="https://resource.51taouk.com/AdminImages/Product/6/76059428-ed5e-41c3-8e37-cd230ec0799d.jpg" />
+                        </div>
+                        <p className="text">【包邮包税】waitrose2019新年大礼包3号</p>
+                        <p className="price">
+                            <span className="active">￥</span><span className="active">20.07</span>
+                            <span className="del"><del>￥24.08</del></span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Mother;
